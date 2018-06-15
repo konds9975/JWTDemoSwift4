97d3bdf3c3c74330ba08c878be0841a803e3574d
Hitendra Bhoir <hitendra@hitendras-mac-mini.local>
2018-06-15 11:24:39 +0530

    Initial Commit

---
+++ JWTDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JWTDemo
+//
+//  Created by Hitendra Bhoir on 15/06/18.
+//  Copyright © 2018 Hitendra Bhoir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
